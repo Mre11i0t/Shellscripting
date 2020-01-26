8c17815014d0ee26ab261a50c86cbacab66c9270
Pavan Karthick M <mpkarthick2002@gmail.com>
2020-01-26 19:43:24 +0530

    Add files via upload

---
+++ Week2/localusers/add-local-user.sh
@@ -0,0 +1,31 @@
+if [[ "${UID}" -eq 0 ]]
+then
+  echo 'You are root.'
+  read -p 'Enter the username to create: ' USER_NAME
+  read -p 'Enter the name of the person who this account : ' COMMENT
+  read -p 'Enter the password to use for the account: ' PASSWORD
+  useradd -c "${COMMENT}" -m ${USER_NAME}
+  if [[ "${?}" -ne 0 ]]
+  then
+    echo 'The useradd command did not execute successfully.'
+    exit 1
+  fi
+  echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+  if [[ "${?}" -ne 0 ]]
+  then
+    echo 'The password command did not execute successfully.'
+    exit 1
+  fi
+  passwd -e ${USER_NAME}
+  echo -e "\nusername:"
+  echo  "${USER_NAME}"
+
+  echo -e "\npassword:"
+  echo "${PASSWORD}"
+
+  echo -e '\nhost:'
+  echo "${HOSTNAME}"
+else
+  echo 'You are not root.'
+  exit 1
+fi
