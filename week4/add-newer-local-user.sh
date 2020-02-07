0a3ef401c45d72a6e69396c92ed69a18aa51e43f
Pavan Karthick M <mpkarthick2002@gmail.com>
2020-02-07 16:35:06 +0530

    Add files via upload

---
+++ week4/add-newer-local-user.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+usage() {
+  echo "NO arguments found use an argument as your user name in the command"
+}
+if [[ "${#}" -eq 0 ]]
+then
+  usage
+  exit 1
+else if [[ "${UID}" -eq 0 ]]
+then
+  USER_NAME=${1}
+  PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c48)
+  useradd -c "${COMMENT}" -m ${USER_NAME}
+  if [[ "${?}" -ne 0 ]]
+  then
+    echo 'The useradd command did not execute successfully.'
+    exit 1
+  fi
+  echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+  if [[ "${?}" -ne 0 ]]
+  then
+    echo 'The password command did not execute successfully.'
+    exit 1
+  fi
+  passwd -e ${USER_NAME}
+  echo -e "\nusername:"
+  echo  "${USER_NAME}"
+
+  echo -e "\npassword:"
+  echo "${PASSWORD}"
+
+  echo -e '\nhost:'
+  echo "${HOSTNAME}"
+else
+  echo 'You are not root.'
+  exit 1
+fi
+fi
